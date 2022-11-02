e6feca3d1b0b7765b8b1daec65b84687fc945cf3
jrsol08 <jrsolomon08@yahoo.com>
2022-11-02 13:34:24 +0800

    ha

--- src/App.js
+++ src/App.js
@@ -130,7 +130,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .Mint(blockchain.account, mintAmount)
+      .mint(blockchain.account, mintAmount)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
