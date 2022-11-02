8a16b5b1f78041604556f5a1211b52b5eed1cb38
jrsol08 <jrsolomon08@yahoo.com>
2022-11-02 21:04:41 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -140,7 +140,7 @@ function App() {
       .once("error", (err) => {
         console.log(err);
         setFeedback("Sorry, something went wrong please try again later.");
-        setClaimingNft(true);
+        setClaimingNft(false);
       })
       .then((receipt) => {
         console.log(receipt);
